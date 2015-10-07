652218fb2f7a060e00058eadd550c89c89fce2c6
Jim <jtulip@csu.edu.au>
2015-10-08 01:35:55 +1100

    refactored AppProperties

--- src/AppProperties.java
+++ src/AppProperties.java
@@ -1,14 +1,36 @@
 
 import java.util.*;
 import java.io.*;
-public class AppProperties {
-private static AppProperties self = null;
-private Properties properties;
 
-public static AppProperties getInstance() {
-if (self == null ) { self = new AppProperties(); } return self;}
-private AppProperties() {properties = new Properties();
-try {properties.load(new FileInputStream("Properties.prop"));} 
-catch (IOException e) {throw new RuntimeException("Could not read property file");}}
-    public Properties getProperties() {return properties;
-}}
+public class AppProperties {
+	
+	private static AppProperties self_ = null;
+	private Properties properties_;
+
+	
+	
+	public static AppProperties getInstance() {
+		if (self_ == null) {
+			self_ = new AppProperties();
+		}
+		return self_;
+	}
+
+	
+	
+	private AppProperties() {
+		properties_ = new Properties();
+		try {
+			properties_.load(new FileInputStream("Properties.prop"));
+		} 
+		catch (IOException e) {
+			throw new RuntimeException("Could not read property file");
+		}
+	}
+
+	
+	
+	public Properties getProperties() {
+		return properties_;
+	}
+}
